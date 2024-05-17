a6f7abdcd629ff81b154c16e61e78875e0433395
DanKohout <daniel.kohout@tul.cz>
2024-05-17 19:05:29 +0200

    first tests and workflows

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+const app = require('./app')
+const port = process.env.PORT || 3000
+
+/**
+ * This function only prints the port number
+ */
+app.listen(port, () => {
+    console.log('server is running on port '+ port)
+})
+
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-
-
-const app = express()
-const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -19,23 +25,23 @@ app.use(express.static(publicDirectoryPath))
 
 
 app.get('', (req, res) => {
-    res.render('index', {
+    res.status(200).render('index', {
         name: 'Daniel Kohout'
     })
 })
 
 
+app.get('/hello', (req, res) => {
+    res.status(200).json({message:'Hello, world!'})
+})
+
 
 
 app.get('*', (req, res) => {
-    res.render('404', {
+    res.status(404).render('404', {
         title: '404',
         name: 'Dakoh Kodah',
         errorMessage: 'Page not found'
     })
 })
 
-
-app.listen(port, () => {
-    console.log('server is running on port '+ port)
-})
